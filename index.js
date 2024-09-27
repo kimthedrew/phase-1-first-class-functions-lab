20aa5f2afafc646fe60f67bb57b0c6a954dade13
kimthedrew <kimutai848@gmail.com>
2024-09-27 09:01:18 +0300

    functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function(drivers) {
+        return drivers.slice(-2);
+    };
+    console.log(returnLastTwoDrivers(drivers));
+    
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ console.log(selectingDrivers[0](drivers));
+ console.log(selectingDrivers[1](drivers));
+ 
+ const createFareMultiplier = function(fareMultiplier) {
+    return function(fare) {
+        return fare * fareMultiplier;
+    };
+ };
+ const fareDoubler = createFareMultiplier(2);
+ const fareTripler = createFareMultiplier(3);
+
+ const selectDifferentDrivers = function(arrayOfDrivers, driverSelectorFunction) {
+    return driverSelectorFunction(arrayOfDrivers);
+ };
+ console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+ console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
